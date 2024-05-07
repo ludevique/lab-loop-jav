f8b0cef5cbd8a222ef26db7532f9c67eda2a7270
ludevique <makamludovique@gmail.com>
2024-05-07 14:39:51 -0700

    loop.javas

---
+++ loops.mjs
@@ -0,0 +1,75 @@
+/* const str = "Hello World";
+
+for (let i = 0; i < str.length; i++) {
+	console.log(str[i]);
+} */
+/* Write while loops to accomplish the following tasks:
+
+Count down to 0 from a given number.
+Log integers in multiples of 3 as long as they are less than 35.
+Print integers in multiples of 5 as long as they are less than 100.
+Print integers between 0 and 20 with the following conditions:
+
+All numbers divisible by 2 should be multiplied by 3 before they are output.
+All other integers should not be output.
+Print all prime numbers between 0 and 20. */
+
+//1
+let num = 6;
+while(num > 0){
+    num--;
+    console.log(num)
+}
+
+//2
+/* let num1 = 6;
+while(num1 < 35){
+    num1 += 3;
+    console.log(num1);
+}
+
+let num2 = 5;
+while(num2 < 100){
+    num2 += 5;
+    console.log(num2);
+}
+
+let num3 = 0;
+while(num3 <= 20){
+    if(num3 % 2 == 0){
+        console.log(num)
+    }
+}
+ */
+// cookie = $4
+
+/* let cookie = 4;
+let bill = 10;
+let result = 10 - 4
+console.log(result)
+
+ *///while(result > 0){
+    result 
+//}
+
+// labeled loop
+/* let x = 1;
+let y = 1;
+
+myLoop: while (true) {
+	console.log(`Outer loop ${x}.`);
+	x++;
+
+	while (true) {
+		console.log(`Inner loop ${y}.`);
+		y++;
+
+		if (x == 5 && y % 5 == 0) {
+			break myLoop;
+		} else if (y % 5 == 0) {
+			break;
+		}
+	}
+} */
+
+
